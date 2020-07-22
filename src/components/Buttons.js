52b9828649250986d7f886657035b453a06937c6
Eric Mauldin <emauldin84@gmail.com>
2020-07-22 12:26:48 -0400

    changes class to className in Button.js and removes superfluous i tags

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -8,14 +8,12 @@ const Buttons = () => {
     const store = React.useContext(StoreContext)
 
     return useObserver(() => (
-        <div class='buttons-container'>
+        <div className='buttons-container'>
             <button type="button" className="btn btn-count" onClick={() => store.updateCount()}>
-                <i className="fa fa-thumbs-o-up" />
                 <FaRegThumbsUp /> Like
             </button>
 
             <button type="button" className="btn btn-count" onClick={() => document.getElementById('comment').focus()}>
-                <i className="fa fa-comment-o" />
                 <FaRegComment /> Comment
             </button>
 
